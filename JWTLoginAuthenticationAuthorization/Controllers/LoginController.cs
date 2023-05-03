4f6154d1be652a6bd9a6bad74ef46a35257d8f90
Chiara <chiaravisca@outlook.com>
2023-05-03 22:48:55 +0200

    Added JWTLoginAuthenticationAuthorization, untested.

---
+++ JWTLoginAuthenticationAuthorization/Controllers/LoginController.cs
@@ -0,0 +1,138 @@
+﻿using JWTLoginAuthenticationAuthorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTLoginAuthenticationAuthorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("validate-token")]
+        public ActionResult ValidateToken(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _config["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _config["Jwt:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return Ok(new { valid = true });
+            }
+            catch
+            {
+                return Ok(new { valid = false });
+            }
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet("token-header")]
+        public ActionResult ValidateToken()
+        {
+            try
+            {
+                // Get the authorization header from the request
+                var authHeader = Request.Headers["Authorization"].FirstOrDefault();
+
+                // Check if the header is present and has the correct format
+                if (string.IsNullOrEmpty(authHeader) || !authHeader.StartsWith("Bearer "))
+                {
+                    return Ok(new { valid = false });
+                }
+
+                // Extract the token from the header
+                var token = authHeader.Substring("Bearer ".Length);
+
+                // Validate the token
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _config["Jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _config["Jwt:Audience"],
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return Ok(new { valid = true });
+            }
+            catch
+            {
+                return Ok(new { valid = false });
+            }
+        }
+
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if (user != null)
+            {
+                var token = GenerateToken(user);
+                return Ok(token);
+            }
+
+            return NotFound("user not found");
+        }
+
+        // To generate token
+        private string GenerateToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Username),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(1500),
+                signingCredentials: credentials);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        //To authenticate user
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(x => x.Username.ToLower() ==
+                userLogin.Username.ToLower() && x.Password == userLogin.Password);
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+            return null;
+        }
+    }
+}
