c438a54ce31a5cb28a4484f0ac7864b55c7ae2c7
Chiara <chiaravisca@outlook.com>
2023-05-04 04:29:31 +0200

    Login page visible but login is still not working.

--- WebSearch/Program.cs
+++ WebSearch/Program.cs
@@ -62,6 +62,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authorization}/{action=Login}/{id?}");
 
 app.Run();
